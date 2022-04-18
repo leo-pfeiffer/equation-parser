dba064943b717d6b5a0b1c98ecdeb97380fb8ec2
leo-pfeiffer <leopold.pfeiffer@icloud.com>
2022-04-18 13:11:52 +0100

    feat: recursive implementation of shunting yard

--- src/main/scala/Parser.scala
+++ src/main/scala/Parser.scala
@@ -6,8 +6,13 @@ def parse(tokens: List[Token]): Expression =
 
 def reversePostfix(tokens: List[Token]): Expression = 
 
+    def isOperator(t: Token): Boolean = t match {
+        case a: OperatorToken => true
+        case _ => false
+    }
+
     def recur(stack: Stack[Expression], tokens: List[Token]): Expression = tokens match {
-        case Nil => return stack.pop
+        case Nil => stack.pop
         case t :: rest => {
             if (isOperator(t)) t match {
                 case t: SumToken => stack.push(Sum(stack.pop, stack.pop))
@@ -29,60 +34,57 @@ def reversePostfix(tokens: List[Token]): Expression =
 
 
 def shuntingYard(tokens: List[Token]): List[Token] = 
-    
-    var i = 0
-    val stack = new Stack[Token]
-    var postfix: List[Token] = List()
 
-    def matchCond(o: OperatorToken): Boolean = stack.head match {
-        case o2: OperatorToken => {
-            (isLeftAssoc(o) && (o.precedence <= o2.precedence)) ||
-            (isRightAssoc(o) && (o.precedence < o2.precedence))
-        }
+    def isLeftAssoc(t: Token): Boolean = t match {
+        case a: Left => true
         case _ => false
     }
 
-    for(token <- tokens) {
-
-        token match {
-            case n: NumberToken => postfix = postfix :+ n
-            case o: OperatorToken => {
-                if (stack.isEmpty) then stack.push(o)
-                else {
-                    while(!stack.isEmpty && matchCond(o)) {
-                        postfix = postfix :+ stack.pop
-                    }
-                    stack.push(o)
-                }
-            }
-            case l: LeftParensToken => stack.push(l)
-            case r: RightParensToken => {
-                while (!isLeftParens(stack.head)) {
-                    postfix = postfix :+ stack.pop
-                }
-                stack.pop
-            }
-        }
+    def isRightAssoc(t: Token): Boolean = t match {
+        case a: Right => true
+        case _ => false
     }
 
-    postfix ++ stack
+    def isLeftParens(t: Token): Boolean = t match {
+        case a: LeftParensToken => true
+        case _ => false
+    }
 
-def isOperator(t: Token): Boolean = t match {
-    case a: OperatorToken => true
-    case _ => false
-}
+    def operatorUpdate(postfix: List[Token], stack: Stack[Token], o: OperatorToken): (Stack[Token], List[Token]) =
+        def matchCond(o: OperatorToken, stack: Stack[Token]): Boolean = stack.head match {
+            case o2: OperatorToken => {
+                (isLeftAssoc(o) && (o.precedence <= o2.precedence)) ||
+                (isRightAssoc(o) && (o.precedence < o2.precedence))
+            }
+            case _ => false
+        }
+        if (stack.isEmpty || !matchCond(o, stack)) (stack.push(o), postfix)
+        else operatorUpdate(postfix :+ stack.pop, stack, o)
 
-def isLeftAssoc(t: Token): Boolean = t match {
-    case a: Left => true
-    case _ => false
-}
+    def rightParensUpdate(postfix: List[Token], stack: Stack[Token]): (Stack[Token], List[Token]) =
+        if (isLeftParens(stack.head)) {stack.pop; (stack, postfix)}
+        else rightParensUpdate(postfix :+ stack.pop, stack)
 
-def isRightAssoc(t: Token): Boolean = t match {
-    case a: Right => true
-    case _ => false
-}
+    def recur(stack: Stack[Token], postfix: List[Token], tokens: List[Token]): List[Token] = 
+        tokens match {
+            case Nil => postfix ++ stack
+            case t :: rest => {
+                t match {
+                    case n: NumberToken => recur(stack, postfix :+ n, rest)
+                    case o: OperatorToken => {
+                        if (stack.isEmpty) then recur(stack.push(o), postfix, rest)
+                        else {
+                            val updated = operatorUpdate(postfix, stack, o)
+                            recur(updated._1, updated._2, rest)
+                        }
+                    }
+                    case l: LeftParensToken => recur(stack.push(l), postfix, rest)
+                    case r: RightParensToken => {
+                        val updated = rightParensUpdate(postfix, stack)
+                        recur(updated._1, updated._2, rest)
+                    }
+                }
+            }
+        }
 
-def isLeftParens(t: Token): Boolean = t match {
-    case a: LeftParensToken => true
-    case _ => false
-}
+    recur(new Stack[Token], List(), tokens)
